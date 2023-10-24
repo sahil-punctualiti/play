3e88e8f8f4c5c8f7ef0f30d9ec91fa83cde0bf4f
sahil <sahil.momin@projectx38.com>
2023-10-24 14:17:57 +0530

    first commit

---
+++ play.js
@@ -0,0 +1,20 @@
+let a = [
+    {
+        helpdeskTicketId:1
+    },
+    {
+        helpdeskTicketId:2
+    },
+    {
+        helpdeskTicketId:3
+    },
+    {
+        helpdeskTicketId:4
+    },
+    {
+        helpdeskTicketId:5
+    }
+]
+
+let b = a.map(i=>i.helpdeskTicketId);
+console.log(b);

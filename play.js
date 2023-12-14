7f7a759a6de7ed73ff2d69a03f4dc367989f2837
sahil <sahil.momin@projectx38.com>
2023-12-14 17:19:45 +0530

    update

--- play.js
+++ play.js
@@ -1,20 +1,9 @@
-let a = [
-    {
-      levelType: 'L1',
-      levelName: 'L1',
-      levelRank: 1,
-      responseTime: 0,
-      resolutionTime: 900000
-    },
-    {
-      levelType: 'L2',
-      levelName: 'L2',
-      levelRank: 2,
-      responseTime: 604800000,
-      resolutionTime: 604800000
-    }
-  ]
+let locationSourceSummary = {
+  "Pritech": {
+    "NA": 2819
+  }
+}
 
-let b= a.reduce((i,j)=>{return (i.responseTime + j.responseTime)})
-
-console.log(b);
+for (const i in locationSourceSummary) {
+  console.log(i);
+}

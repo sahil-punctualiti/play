a3409b4e50aef5464130ab3bca1be67f725e1703
sahil <sahil.momin@projectx38.com>
2024-02-27 18:16:27 +0530

    update

--- sahil.js
+++ sahil.js
@@ -1,24 +1,7 @@
-{
-  "areaId": "4c4aa4eb-93b0-4866-951f-c3978546e873",
-  "siteId": "4c4aa4eb-93b0-4866-951f-c3978546e873",
-  "buildingId": "NA",
-  "isRootArea": true,
-  "areaName": "OHC",
-  "areaType": "SITE",
-  "parentArea": {
-    "parentAreaType": "SELF",
-    "parentAreaId": "4c4aa4eb-93b0-4866-951f-c3978546e873",
-    "parentAreaName": "Infinity"
-  },
-  "parentsHararchyArray": [
-    "4c4aa4eb-93b0-4866-951f-c3978546e873"
-  ],
-  "status": "ACTIVE",
-  "createdDate": {
-    "epochDate": 1705034931000,
-    "isoDate": {
-      "$date": "2024-01-12T04:48:51.000Z"
-    }
-  },
-  "__v": 0
+let a = "hi"
+
+let b = {
 }
+b[`${a}`] = 1
+
+console.log(b);
